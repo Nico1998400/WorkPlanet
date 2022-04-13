4dfbee42af5892c312f062eb90241e216fd35f8d
Nico1998400 <Nico1998@live.se>
2022-04-13 12:13:46 +0200

    Done

--- src/main/java/com/example/workplanet/security/SecurityConfig.java
+++ src/main/java/com/example/workplanet/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,6 +29,11 @@ public class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         setLoginView(http, LoginView.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception { // En Override för att komma in i databasen H2.
+        web.ignoring().antMatchers("/h2-console/**");
     }
 
     @Override
